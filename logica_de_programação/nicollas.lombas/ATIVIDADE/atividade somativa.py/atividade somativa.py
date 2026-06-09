4a2a5f5e647fa25d04ac456517ebe2328aa0f2a6
Nicolas Lombas <nickmaru56@gmail.com>
2026-06-09 09:39:27 -0300

    atividade avaliativa

---
+++ logica_de_programação/nicollas.lombas/ATIVIDADE/atividade somativa.py/atividade somativa.py
@@ -0,0 +1,156 @@
+# Foco: print, input, operações matemáticas e f-strings
+# 1. Registro de Operador: Peça o nome do operador e o turno (A, B ou C). Exiba:
+# "Operador [Nome] registrado no Turno [Turno]. Boa jornada!"
+# import tkinter as tk
+# from tkinter import messagebox
+
+
+# def janela_bemvindo():
+#     nome = nome_usuario.get().strip()
+#     turno = turno_usuario.get().strip().upper()  
+    
+#     if nome == "":
+#         messagebox.showwarning("Aviso", "Por favor, digite seu nome :)")
+#         return
+        
+#     if turno == "" or turno not in ["A", "B", "C"]:
+#         messagebox.showwarning("Aviso", "Por favor, digite um turno válido (A, B ou C) :)")
+#         return
+        
+    
+#     mensagem_final = f"Operador {nome} registrado no Turno {turno}. Boa jornada!"
+#     messagebox.showinfo("Registro Concluído", mensagem_final)
+
+
+# janela = tk.Tk()
+# janela.title("Registro de Operador")
+# janela.geometry("350x200")
+# janela.configure(bg="black")
+
+# lbl_nome = tk.Label(janela, text="Nome do Operador:", bg="blue", font=("Arial", 10, "bold"))
+# lbl_nome.grid(row=0, column=0, pady=10, padx=10, sticky="w")
+
+# nome_usuario = tk.Entry(janela, font=("Arial", 12))
+# nome_usuario.grid(row=0, column=1, pady=10, padx=10)
+
+
+# lbl_turno = tk.Label(janela, text="Turno (A, B ou C):", bg="blue", font=("Arial", 10, "bold"))
+# lbl_turno.grid(row=1, column=0, pady=10, padx=10, sticky="w")
+
+# turno_usuario = tk.Entry(janela, font=("Arial", 12))
+# turno_usuario.grid(row=1, column=1, pady=10, padx=10)
+
+
+# btn_mensagem = tk.Button(janela, text="Registrar", command=janela_bemvindo, font=("Arial", 10, "bold"))
+# btn_mensagem.grid(row=2, column=0, columnspan=2, pady=15)
+
+# janela.mainloop()
+
+
+#. 2 Cálculo de Produção: Peça a quantidade de peças produzidas em 1 hora. Calcule e
+#exiba quantas peças serão produzidas em um turno de 8 horas.
+
+# import tkinter as tk
+# from tkinter import messagebox
+
+# def calcular_producao():
+    
+#     pecas_por_hora_n = pecas_hora.get()
+
+    
+#     if pecas_por_hora_n == "":
+#         messagebox.showwarning("Aviso", "Preencha a quantidade de peças!")
+#         return
+
+    
+#     try:
+#         pecas_por_hora = int(pecas_por_hora_n)
+#     except ValueError:
+#         messagebox.showerror("Erro", "Digite apenas números inteiros para as peças.")
+#         return
+
+    
+#     turno_horas = 8
+#     total_produzido = pecas_por_hora * turno_horas
+
+    
+#     messagebox.showinfo(
+#         "Resultado da Produção",
+#         f"Produção por hora: {pecas_por_hora} peças\n"
+#         f"Duração do turno: {turno_horas} horas\n"
+#         f"Total produzido no turno: {total_produzido} peças"
+#     )
+
+
+# janela = tk.Tk()
+# janela.title("Cálculo de Produção")
+# janela.geometry("450x200")
+# janela.configure(bg="lightblue")
+
+
+# lbl_pecas = tk.Label(janela, text="Peças produzidas em 1 hora:")
+# lbl_pecas.grid(row=0, column=0, padx=10, pady=20)
+
+
+# pecas_hora = tk.Entry(janela)
+# pecas_hora.grid(row=0, column=1, padx=10, pady=20)
+
+# btn_calcular = tk.Button(
+#     janela,
+#     text="Calcular Turno (8h)",
+#     command=calcular_producao
+# )
+# btn_calcular.grid(row=1, column=0, columnspan=2, pady=20)
+
+
+# janela.mainloop()
+
+# 3.Conversor de Unidade: O sistema lê uma pressão em Bar. Converta para PSI (1 Bar
+# ≈ 14.5 PSI) e exiba com duas casas decimais.
+
+import tkinter as tk
+from tkinter import messagebox
+
+def calcular_producao():
+    
+    bar = bar.get()
+
+    
+    if bar == "":
+        messagebox.showwarning("Aviso", "digite a quantidade de bar")
+        return
+
+    
+    
+
+    
+    PSI = 14.5
+    total_produzido = bar * PSI 
+
+    
+   
+    )
+
+
+janela = tk.Tk()
+janela.title("Cálculo de Produção")
+janela.geometry("450x200")
+janela.configure(bg="lightblue")
+
+
+lbl_pecas = tk.Label(janela, text="Total de PSI")
+lbl_pecas.grid(row=0, column=0, padx=10, pady=20)
+
+
+pecas_hora = tk.Entry(janela)
+pecas_hora.grid(row=0, column=1, padx=10, pady=20)
+
+btn_calcular = tk.Button(
+    janela,
+    text="Calcular PSI (14.5)",
+    command=calcular_producao
+)
+btn_calcular.grid(row=1, column=0, columnspan=2, pady=20)
+
+
+janela.mainloop()
